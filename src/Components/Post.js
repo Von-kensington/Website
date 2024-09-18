f5dae6589340b84d1273b0dbb4d1486370366b18
Vonkensington <vonkensington@gmail.com>
2024-09-17 22:27:31 -0500

    Post Creation Divs

---
+++ src/Components/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Post.css'
+
+function Post({ username, imageUrl, message }) {
+    return (
+        <div>
+            <div className="postHeader">
+                sadfafdwa
+            </div>
+            <div className="postImage">
+                sdjlfjalfkjea
+            </div>
+            safdsfsafda
+            <div className="postMessage"></div>
+        </div>
+    )
+}
+
+export default Post
